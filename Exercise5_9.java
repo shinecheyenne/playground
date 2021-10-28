8e4d2091f4380649413a3ae184bd61f96d0f44ba
Cheyenne Shin <duma.abyssinia@gmail.com>
2021-10-28 17:09:36 +0900

    Add files via upload

---
+++ Exercise5_9.java
@@ -0,0 +1,38 @@
+
+public class Exercise5_9 {
+
+	public static void main(String[] args) {
+		char[][] star = {
+				{'*','*',' ',' ',' '},
+				{'*','*',' ',' ',' '},
+				{'*','*','*','*','*'},
+				{'*','*','*','*','*'}
+		};
+		
+		char[][] result = new char[star[0].length][star.length];
+		
+		for(int i=0;i<star.length;i++) {
+			for(int j=0;j<star[i].length;j++) {
+				System.out.print(star[i][j]);
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i=0;i<star.length;i++) {
+			for(int j=0;j<star[i].length;j++) {
+				System.out.println("asdf");
+				result[i][j]=star[3-j][i];
+			}
+		}
+		
+		for(int i=0;i<result.length;i++) {
+			for(int j=0;j<result[i].length;j++) {
+				System.out.print(result[i][j]);
+			}
+			System.out.println();
+			
+		}
+	}
+}
